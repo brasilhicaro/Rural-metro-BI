f16ed91367c917e25bc6a6a626aec001fef1b5d7
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-06-18 19:50:53 -0300

    :bug: fix: solve sum problem

--- etl_doacao_direta_autos_de_infracao_ibama_2018.py
+++ etl_doacao_direta_autos_de_infracao_ibama_2018.py
@@ -2,13 +2,10 @@ import pandas as pd
 
 df = pd.read_excel('./data/doacoes_diretas_vindas_pessoas_com_autos_de_infracao_ibama_2018.xlsx')
 df = df.drop(columns=['entidade_id_mascarada', 'origem_do_dinheiro_no_tse', 'data', 'nome_deputado'])
-
 df["valor"] = df['valor'].str.replace(",", ".")
 
 df["valor"] = df["valor"].astype(float)
-# ta dando erro aqui, não tá conseguindo somar os valores
-# A tabela tá saindo de acordo com a posição dos elementos no groupby, caso queira mudar o formato
-# só modificar a ordem
-df = df.groupby(['cpf_deputado', 'cpf_doador', 'nome_doador','doacoes_tipo', 'nome_urna', 'valor']).sum().reset_index()
+
+df = df.groupby(['cpf_deputado', 'cpf_doador', 'nome_doador','doacoes_tipo', 'nome_urna'])['valor'].sum().reset_index()
 df.to_csv('./result_data/resultados_doacao_direta_infracao_ibama_2018.csv')
 
