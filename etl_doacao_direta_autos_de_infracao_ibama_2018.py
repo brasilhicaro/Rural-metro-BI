7b79e3b8d9dbfa93da4904b9b570d63cac205c9a
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-06-18 12:34:11 -0300

    :construction: build: adding elt in donation in 2018
    falta ainda juntar com os dados dos outros arquivos

---
+++ etl_doacao_direta_autos_de_infracao_ibama_2018.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_excel('./data/doacoes_diretas_vindas_pessoas_com_autos_de_infracao_ibama_2018.xlsx')
+df = df.drop(columns=['entidade_id_mascarada', 'origem_do_dinheiro_no_tse', 'data', 'nome_deputado'])
+
+df["valor"] = df['valor'].str.replace(",", ".")
+
+df["valor"] = df["valor"].astype(float)
+df = df.groupby(['cpf_deputado', 'cpf_doador', 'nome_doador','doacoes_tipo', 'nome_urna', 'valor']).sum().reset_index()
+df.to_csv('./result_data/resultados_doacao_direta_infracao_ibama_2018.csv')
+
--- main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel(r'./data/cadastro_de_empregadores.xlsx', sheet_name='cadastro_de_empregadores')
-df.to_csv(r'./data/cadastro_de_empregadores.xlsx', index=None, header=True)
-
-print(df.head())
