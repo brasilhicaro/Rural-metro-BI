82b602f0c089aa307521b4510ebf5a906ef1947f
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-06-18 12:46:45 -0300

    :bulb: docs: add comments in etl file

--- etl_doacao_direta_autos_de_infracao_ibama_2018.py
+++ etl_doacao_direta_autos_de_infracao_ibama_2018.py
@@ -6,6 +6,9 @@ df = df.drop(columns=['entidade_id_mascarada', 'origem_do_dinheiro_no_tse', 'dat
 df["valor"] = df['valor'].str.replace(",", ".")
 
 df["valor"] = df["valor"].astype(float)
+# ta dando erro aqui, não tá conseguindo somar os valores
+# A tabela tá saindo de acordo com a posição dos elementos no groupby, caso queira mudar o formato
+# só modificar a ordem
 df = df.groupby(['cpf_deputado', 'cpf_doador', 'nome_doador','doacoes_tipo', 'nome_urna', 'valor']).sum().reset_index()
 df.to_csv('./result_data/resultados_doacao_direta_infracao_ibama_2018.csv')
 
